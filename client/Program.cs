4d8199decb03c8d16dc4eccd4ebae9ed2367061b
Jason De Lorme <delormej@gmail.com>
2020-11-17 21:02:10 -0500

    Initial commit, working with grpc auth.

---
+++ client/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc;
+using Grpc.Net.Client;
+using Grpc.Core;
+using Grpc.Auth;
+using Google.Apis.Auth.OAuth2;
+using cloudrun;
+
+namespace client
+{
+    class Program
+    {
+        // const string serverAddress = "http://localhost:5000";
+        // const string serverAddress = "http://34.122.133.56";
+        const string serverAddress = "https://mygrpc-62b5pp6dlq-uc.a.run.app";
+
+        static async Task Main(string[] args)
+        {
+            // var oidcOptions = OidcTokenOptions.FromTargetAudience(serverAddress);
+
+            // // see: https://grpc.github.io/grpc/csharp/api/Grpc.Auth.GoogleGrpcCredentials.html
+
+            GoogleCredential creds = GoogleCredential.GetApplicationDefault();
+            var oidcToken = await creds.GetOidcTokenAsync(
+                OidcTokenOptions.FromTargetAudience(serverAddress)
+            );
+            var bearerToken = await oidcToken.GetAccessTokenAsync();
+            var headers = new Metadata();
+            headers.Add("Authorization", $"Bearer {bearerToken}");
+
+            var channel = GrpcChannel.ForAddress(serverAddress);
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                new HelloRequest {Name = "Jason"}, headers 
+            );
+            System.Console.WriteLine("Greeting " + reply.Message);           
+        }
+    }
+}
